e41032f4b0938b3e40bd06c5f45b41d7d4c49222
GaetanKarst <gaetan.karst@gmail.com>
2021-05-18 00:14:14 +0900

    added toast when name input field empty

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,15 @@ class MainActivity : AppCompatActivity() {
 
         // Hide status bar for immersive purpose (method deprecated on Android30)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        val startButton = findViewById<Button>(R.id.start_button)
+        val etName = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.et_name)
+
+        startButton.setOnClickListener {
+            if (etName.text.toString().isEmpty()){
+                println("Empty input field!")
+                Toast.makeText(this, "Enter your name to start!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
