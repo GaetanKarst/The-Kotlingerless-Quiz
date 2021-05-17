9d52bb622f79e3345ba82a376bc9738335ea2f4d
GaetanKarst <gaetan.karst@gmail.com>
2021-05-17 22:55:31 +0900

    1st screen UI done

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.quiz_app
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Hide status bar for immersive purpose (method deprecated on Android30)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
