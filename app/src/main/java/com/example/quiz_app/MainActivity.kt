580275d6fc3601b4bd96c6a94946e91b51f4f64b
GaetanKarst <gaetan.karst@gmail.com>
2021-05-18 11:40:33 +0900

    xml styling, but app bar to resolve later

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -9,17 +9,22 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        super. onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Hide status bar for immersive purpose (method deprecated on Android30)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
+        startButtonListen();
+
+    }
+
+    private fun startButtonListen() {
         val startButton = findViewById<Button>(R.id.start_button)
         val etName = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.et_name)
 
         startButton.setOnClickListener {
-            if (etName.text.toString().isEmpty()){
+            if (etName.text.toString().isEmpty()) {
                 println("Empty input field!")
                 Toast.makeText(this, "Enter your name to start!", Toast.LENGTH_SHORT).show()
             }
