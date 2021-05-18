9482f6a0b62a13751b9fec0e45663d6431b955d9
GaetanKarst <gaetan.karst@gmail.com>
2021-05-18 16:57:55 +0900

    anwers displaying and can be clicked

--- app/src/main/java/com/example/quiz_app/QuestionActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuestionActivity.kt
@@ -2,6 +2,8 @@ package com.example.quiz_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.widget.Button
 import android.widget.TextView
 
 class QuestionActivity : AppCompatActivity() {
@@ -9,15 +11,29 @@ class QuestionActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_question)
 
+        val questionIndex: Int = 0;
+        val score: Int = 0;
         val questionList = Constants.getQuestions();
 
-        displayQuestion1(questionList);
+        displayQuestion(questionList, questionIndex, score);
     }
 
-    private fun displayQuestion1(list : ArrayList<Question>) : Unit {
+    private fun displayQuestion(list : ArrayList<Question>, index: Int, score: Int) : Unit {
         val questionText = findViewById<TextView>(R.id.question_text);
-        questionText.setText(list.get(0).question);
-        println(list.get(0));
+        val questionAnswer1 = findViewById<TextView>(R.id.question_answer_1);
+        val questionAnswer2 = findViewById<TextView>(R.id.question_answer_2);
+        val questionAnswer3 = findViewById<TextView>(R.id.question_answer_3);
+        val questionAnswer4 = findViewById<TextView>(R.id.question_answer_4);
 
+        questionText.setText(list.get(index).question);
+        questionAnswer1.setText(list.get(index).answerOne);
+        questionAnswer1.setOnClickListener() {
+            println("Answer1 clicked 🦉")
+        }
+        questionAnswer2.setText(list.get(index).answerTwo);
+        questionAnswer3.setText(list.get(index).answerThree);
+        questionAnswer4.setText(list.get(index).answerFour);
     }
+
+    //TODO: Put the onResume function here to handle right answer and increase question index + score
 }
