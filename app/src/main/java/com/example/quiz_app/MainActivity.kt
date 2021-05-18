50d60745986aced93f3f94de604667561b0b0b85
GaetanKarst <gaetan.karst@gmail.com>
2021-05-18 12:33:11 +0900

    question design + get players's name across activities

--- app/src/main/java/com/example/quiz_app/MainActivity.kt
+++ app/src/main/java/com/example/quiz_app/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.quiz_app
 
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.security.KeyChain.EXTRA_NAME
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -20,14 +23,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startButtonListen() {
-        val startButton = findViewById<Button>(R.id.start_button)
-        val etName = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.et_name)
+        val startButton = findViewById<Button>(R.id.start_button);
+        val etName = findViewById<androidx.appcompat.widget.AppCompatEditText>(R.id.et_name);
 
         startButton.setOnClickListener {
             if (etName.text.toString().isEmpty()) {
-                println("Empty input field!")
-                Toast.makeText(this, "Enter your name to start!", Toast.LENGTH_SHORT).show()
+                println("Empty input field!");
+                Toast.makeText(this, "Enter your name to start!", Toast.LENGTH_SHORT).show();
+            } else {
+                // Gets players' input name to pass it across activities and Finished the current activity
+                getPlayerName();
+                finish();
             }
         }
+    }
+
+    private fun getPlayerName() {
+        val nameField = findViewById<EditText>(R.id.et_name);
+        val name = nameField.text.toString();
+        val intent = Intent(this, QuestionActivity::class.java).apply {
+            putExtra(EXTRA_NAME, name);
+        }
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/quiz_app/Question.kt
@@ -0,0 +1,13 @@
+package com.example.quiz_app
+
+data class Question (
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val answerOne: String,
+    val answerTwo: String,
+    val answerThree: String,
+    val answerFour: String,
+
+
+)
