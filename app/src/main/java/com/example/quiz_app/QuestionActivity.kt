75762baaf5548bab411559dcbb926bd44c24383f
GaetanKarst <gaetan.karst@gmail.com>
2021-05-18 17:51:39 +0900

    answers displaying working on logic right answer

--- app/src/main/java/com/example/quiz_app/QuestionActivity.kt
+++ app/src/main/java/com/example/quiz_app/QuestionActivity.kt
@@ -20,19 +20,29 @@ class QuestionActivity : AppCompatActivity() {
 
     private fun displayQuestion(list : ArrayList<Question>, index: Int, score: Int) : Unit {
         val questionText = findViewById<TextView>(R.id.question_text);
-        val questionAnswer1 = findViewById<TextView>(R.id.question_answer_1);
-        val questionAnswer2 = findViewById<TextView>(R.id.question_answer_2);
-        val questionAnswer3 = findViewById<TextView>(R.id.question_answer_3);
-        val questionAnswer4 = findViewById<TextView>(R.id.question_answer_4);
+        val answer1 = findViewById<TextView>(R.id.question_answer_1);
+        val answer2 = findViewById<TextView>(R.id.question_answer_2);
+        val answer3 = findViewById<TextView>(R.id.question_answer_3);
+        val answer4 = findViewById<TextView>(R.id.question_answer_4);
 
+        // TODO: Make it button but can't figure out how to set the text of an XML button element
         questionText.setText(list.get(index).question);
-        questionAnswer1.setText(list.get(index).answerOne);
-        questionAnswer1.setOnClickListener() {
-            println("Answer1 clicked 🦉")
+        answer1.setText(list.get(index).answerOne)
+        answer1.setOnClickListener() {
+            println("Answer 1 clicked")
         }
-        questionAnswer2.setText(list.get(index).answerTwo);
-        questionAnswer3.setText(list.get(index).answerThree);
-        questionAnswer4.setText(list.get(index).answerFour);
+        answer2.setText(list.get(index).answerTwo);
+        answer2.setOnClickListener() {
+            println("Answer 2 clicked")
+        }
+        answer3.setText(list.get(index).answerThree);
+        answer3.setOnClickListener() {
+            println("Answer 3 clicked")
+        }
+        answer4.setText(list.get(index).answerFour);
+        answer4.setOnClickListener() {
+            println("Answer 4 clicked")
+        }
     }
 
     //TODO: Put the onResume function here to handle right answer and increase question index + score
